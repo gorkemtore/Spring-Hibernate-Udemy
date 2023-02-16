87ffb5508387bb2a25f2a6a14328bb3c207ebb3f
Gorkem <gorkemtore1@gmail.com>
2023-02-17 02:07:31 +0300

    spring öncesi practice.

---
+++ spring-demo-one/src/com/lux2code/springdemo/BaseballCoach.java
@@ -0,0 +1,9 @@
+package com.lux2code.springdemo;
+
+public class BaseballCoach implements Coach{
+
+	@Override
+	public String getDailyWorkout() {
+		return "Spend 30 minutes in batting practice";
+	}
+}
---
+++ spring-demo-one/src/com/lux2code/springdemo/Coach.java
@@ -0,0 +1,6 @@
+package com.lux2code.springdemo;
+
+public interface Coach {
+	public String getDailyWorkout();
+
+}
